63a5b45a137d77b5f50ad65cc88341623e8d1cc2
buddy <buddy.m091@gmail.com>
2021-02-15 17:03:47 -0400

    Fixed bugs

--- src/controllers/services.controller.js
+++ src/controllers/services.controller.js
@@ -3,7 +3,8 @@ const servicesCtrl = {};
 const pool = require('../database');
 
 servicesCtrl.renderServices = async (req, res) => {
-    const services = await pool.query('SELECT * FROM services WHERE deleted_at is NULL');
+    const services = await pool.query(` SELECT s.*, d.name as driver, c.name as customer FROM services as s, drivers as d, customer_locations as l, customers as c
+                                        WHERE s.driver_id = d.id and s.location_id = l.id and l.customer_id = c.id and s.deleted_at is NULL`);
     res.render('services/list', { services });
 }
 
